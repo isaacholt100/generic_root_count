ac17f42d0897d72e378310826b1c47db4d65df36
Yue Ren <yue.ren.kl@gmail.com>
2024-02-23 17:52:14 +0000

    chg: updated source, removed toml files

--- src/main.jl
+++ src/main.jl
@@ -1,13 +1,13 @@
 using Oscar
 using MixedSubdivisions
 
-function base_coefficient_rings(F::Vector{<:MPolyElem})
+function base_coefficient_rings(F::Vector{<:MPolyRingElem})
     R = parent(first(F))
     A = coefficient_ring(R)
     return (R, A)
 end
 
-function collect_terms(F::Vector{<:MPolyElem})
+function collect_terms(F::Vector{<:MPolyRingElem})
     (_, A) = base_coefficient_rings(F)
     # this function assumes that the system is horizontally parametrized
     dicts = Vector{Dict}()
@@ -41,7 +41,7 @@ function factorise_terms(d::Dict)
     return dict
 end
 
-function factorize_collected_terms(F::Vector{<:MPolyElem})
+function factorize_collected_terms(F::Vector{<:MPolyRingElem})
     # take the vector of dictionaries returned from collect_terms, and map it to a vector of dictionaries, with the same keys, but now the polynomial values are factorised
     collected_terms = collect_terms(F)
     return [factorise_terms(t) for t in collected_terms]
@@ -60,7 +60,7 @@ function tropical_base(factorized_terms::Vector{<:Dict})
     return unique(B)
 end
 
-function modified_support(F::Vector{<:MPolyElem})
+function modified_support(F::Vector{<:MPolyRingElem})
     # compute the support of the modification of the system, in the format that MixedSubdivision.jl will accept
     (R, A) = base_coefficient_rings(F)
     factorized_terms = factorize_collected_terms(F)
@@ -108,12 +108,12 @@ function nonlinear_resonator_system(n::Int, m::Int; specialized::Bool = false)
             ["a" * string(i) for i in 0:n+2],
             ["b" * string(i) for i in 0:n+2]
         )
-        A, ab = PolynomialRing(QQ, ab_string)
+        A, ab = polynomial_ring(QQ, ab_string)
         a = ab[1:div(length(ab), 2)]
         b = ab[div(length(ab), 2)+1:end]
     end
 
-    R, (u, v) = PolynomialRing(A, ["u", "v"])
+    R, (u, v) = polynomial_ring(A, ["u", "v"])
     f = a[1] + a[2] * u + a[3] * v + sum([a[i+3] * u * (u^m + v^m)^i for i in 1:n])
     g = b[1] + b[2] * u + b[3] * v + sum([b[i+3] * v * (u^m + v^m)^i for i in 1:n])
 
@@ -121,11 +121,11 @@ function nonlinear_resonator_system(n::Int, m::Int; specialized::Bool = false)
 end
 
 
-function generic_root_count(system::Vector{<:MPolyElem})
+function generic_root_count(system::Vector{<:MPolyRingElem})
     support = modified_support(system)
     return mixed_volume(support)
 end
 
 n = 2
 m = 3
-@assert generic_root_count(nonlinear_resonator_polynomials(n, m)) == 2 * m * n + 1
+@assert generic_root_count(nonlinear_resonator_system(n, m)) == 2 * m * n + 1
